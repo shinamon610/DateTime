f70079f00deb104ec38a181a4e9dbf34c72fe595
shinamon <76248816+shinamon610@users.noreply.github.com>
2025-03-10 17:00:30 +0900

    make this compile in nixos

--- flake.nix
+++ flake.nix
@@ -4,8 +4,21 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   outputs = { self, nixpkgs }:
-    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      stdLibPath = "${pkgs.stdenv.cc.cc.lib}/lib";
     in {
-      devShell.x86_64-linux = pkgs.mkShell { buildInputs = [ pkgs.elan ]; };
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = [ pkgs.elan pkgs.stdenv.cc.cc.lib ];
+
+        # NIX_SSL_CERT_FILE を無効にする
+        shellHook = ''
+          export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${stdLibPath}
+          mkdir -p $PWD/.nix-shell/lib
+          ln -sf ${stdLibPath}/libstdc++.so.6 $PWD/.nix-shell/lib/
+          export PATH=$PATH:$PWD/.nix-shell/lib
+          unset NIX_SSL_CERT_FILE
+        '';
+      };
     };
 }
